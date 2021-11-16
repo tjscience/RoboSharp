dbed16378ab6ae7b86abd3cf79dbf0ad9148fd30
RFBomb <rzbrenckman@gmail.com>
2021-11-16 11:09:00 -0500

    Add the AverageStatistics method to the SpeedStatistic object

--- RoboSharp/Results/SpeedStatistic.cs
+++ RoboSharp/Results/SpeedStatistic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace RoboSharp.Results
 {
@@ -44,5 +45,68 @@ namespace RoboSharp.Results
 
             return res;
         }
+
+        #region ADD
+
+        //The Adding methods exists solely to facilitate the averaging method. Adding speeds together over consecutive runs makes little sense. 
+
+        /// <summary>
+        /// Add the results of the supplied SpeedStatistic objects to this Statistics object.
+        /// </summary>
+        /// <param name="stats">Statistics Items to add</param>
+        private void AddStatistic(IEnumerable<SpeedStatistic> stats)
+        {
+            foreach (SpeedStatistic stat in stats)
+            {
+                this.BytesPerSec += stat.BytesPerSec;
+                this.MegaBytesPerMin += stat.MegaBytesPerMin;
+            }
+        }
+
+        /// <summary>
+        /// Combine the results of the supplied SpeedStatistic objects
+        /// </summary>
+        /// <param name="stats">SpeedStatistic Items to add</param>
+        /// <returns>New Statistics Object</returns>
+        private static SpeedStatistic AddStatistics(IEnumerable<SpeedStatistic> stats)
+        {
+            SpeedStatistic ret = new SpeedStatistic();
+            ret.AddStatistic(stats);
+            return ret;
+        }
+
+        #endregion ADD
+
+        #region AVERAGE
+
+        /// <summary>
+        /// Combine the supplied <see cref="SpeedStatistic"/> objects, then get the average.
+        /// </summary>
+        /// <param name="stats">Array of Stats objects</param>
+        public void AverageStatistic(IEnumerable<SpeedStatistic> stats)
+        {
+            this.AddStatistic(stats);
+            int cnt = stats.Count() + 1;
+            BytesPerSec /= cnt;
+            MegaBytesPerMin /= cnt;
+
+        }
+
+        /// <returns>New Statistics Object</returns>
+        /// <inheritdoc cref=" AverageStatistic(IEnumerable{SpeedStatistic})"/>
+        public static SpeedStatistic AverageStatistics(IEnumerable<SpeedStatistic> stats)
+        {
+            SpeedStatistic stat = AddStatistics(stats);
+            int cnt = stats.Count();
+            if (cnt > 1)
+            {
+                stat.BytesPerSec /= cnt;
+                stat.MegaBytesPerMin /= cnt;
+            }
+            return stat;
+        }
+
+        #endregion AVERAGE
+
     }
 }
