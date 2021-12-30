bf0d67493f33f373bb71e5ed56f140837e267f7a
Robert B <Robbery525@gmail.com>
2021-12-30 14:58:48 -0500

    More accurate summary tags

--- RoboSharp/ProcessedFileInfo.cs
+++ RoboSharp/ProcessedFileInfo.cs
@@ -5,7 +5,7 @@
     /// </summary>
     public enum FileClassType
     {
-        /// <summary>Details about a FOLDER</summary>
+        /// <summary>Details about a Directory</summary>
         NewDir,
         /// <summary>Details about a FILE</summary>
         File,
@@ -18,10 +18,17 @@
     {
         /// <summary>Description of the item as reported by RoboCopy</summary>
         public string FileClass { get; set; }
+        
         /// <inheritdoc cref="RoboSharp.FileClassType"/>
         public FileClassType FileClassType { get; set; }
-        /// <summary>File Size</summary>
+
+        /// <summary>
+        /// File -> File Size <br/>
+        /// Directory -> Number files in folder -> Can be negative if PURGE is used <br/>
+        /// SystemMessage -> Should be 0
+        /// </summary>
         public long Size { get; set; }
+
         /// <summary>Folder or File Name / Message Text</summary>
         public string Name { get; set; }
     }
