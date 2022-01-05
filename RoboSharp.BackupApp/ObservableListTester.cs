bc32f49becbe492aefc58de5d0dc97b77160dfb9
Robert Brenckman <Robbery525@gmail.com>
2022-01-05 10:44:04 -0500

    Observable list fix (#132)
    * Rename ObservableList File
    
    * Validate ObservableList INotifyCollectionChanged
    
    * Add Usage Remarks for ProgressEstimator
    
    * Include ObservableListTester in Project

---
+++ RoboSharp.BackupApp/ObservableListTester.cs
@@ -0,0 +1,215 @@
+﻿using RoboSharp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace RoboSharp.BackupApp
+{
+    /// <summary>
+    /// Test <see cref="ObservableList{T}.CollectionChanged"/> event
+    /// </summary>
+    internal class ObservableListTester
+    {
+        public ObservableListTester()
+        {
+            OList = new ObservableList<int>();
+            OList.CollectionChanged += List_CollectionChanged;
+        }
+
+        private EventHandler SetNextExpectation;
+
+        private readonly ObservableList<int> OList;
+        private NotifyCollectionChangedAction ExpectedAction;
+        private int NewItemsCount;
+        private int OldItemsCount;
+        private int NewStartingIndex;
+        private int OldStartingIndex;
+        private bool DidEventFire;
+        private int EventsFiredCount;
+        private int ExpectedEventsFiredCount;
+
+        private void SetAddExpectation(int Count = 1, int StartingIndex = -1) => SetExpectations(NotifyCollectionChangedAction.Add, Count, 0, StartingIndex);
+        private void SetMoveExpectation(int Count, int StartingIndex) => SetExpectations(NotifyCollectionChangedAction.Move, Count, 0, StartingIndex);
+        private void SetRemoveExpectation(int Count, int StartingIndex) => SetExpectations(NotifyCollectionChangedAction.Remove, 0, Count, -1, StartingIndex);
+        private void SetReplaceExpectation(int Count = 1, int StartingIndex = -1) => SetExpectations(NotifyCollectionChangedAction.Replace, Count, Count, StartingIndex, StartingIndex);
+
+        private void SetInsertExpectations(int count, int StartingIndex, int ExpectedMoveEvents = 1)
+        {
+            SetAddExpectation(count, StartingIndex);
+            SetNextExpectation += (_, __) => SetMoveExpectation(ExpectedMoveEvents, StartingIndex + count);
+        }
+
+        private void SetExpectations(NotifyCollectionChangedAction action, int NewCount, int OldCount, int NewIndex = -1, int OldIndex = -1)
+        {
+            if (!DidEventFire)
+                throw new Exception("Previous Event did not fire!");
+
+            ExpectedAction = action;
+            NewItemsCount = NewCount;
+            OldItemsCount = OldCount;
+            NewStartingIndex = NewIndex;
+            OldStartingIndex = OldIndex;
+            EventsFiredCount = 0;
+            ExpectedEventsFiredCount = action == NotifyCollectionChangedAction.Move ? NewCount : 1;
+            DidEventFire = false;
+        }
+
+        private void ThrowIfCountUnexpected(string TestID)
+        {
+            if (ExpectedEventsFiredCount != EventsFiredCount)
+                throw new Exception($"Test ID: {TestID}\nExpectedEventsFiredCount != EventsFiredCount\nExpected:{ExpectedEventsFiredCount }\nReceived:{EventsFiredCount}");
+        }
+
+        private void List_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+
+            if (e.Action != ExpectedAction)
+                throw new Exception("NotifyCollectionChangedAction Not Of Expected Type");
+
+            bool CheckAdd = false;
+            bool CheckRemove = false;
+
+            EventsFiredCount++;
+            if (e.Action == NotifyCollectionChangedAction.Move) { }
+            else if (e.Action == NotifyCollectionChangedAction.Reset) { }
+            else if (e.Action == NotifyCollectionChangedAction.Add) { CheckAdd = true; }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)  { CheckRemove = true; }
+            else if (e.Action == NotifyCollectionChangedAction.Replace) { CheckAdd = true; CheckRemove = true; }
+            else { }
+
+            if (CheckAdd)
+            {
+                if (NewItemsCount != e.NewItems.Count) throw new Exception($"NewItems.Count != Expected Count\nExpected:{NewItemsCount}\nReceived:{e.NewItems.Count}");
+                if (NewStartingIndex >= 0 && NewStartingIndex != e.NewStartingIndex) 
+                    throw new Exception($"NewStartingIndex != Expected Index\nExpected:{NewStartingIndex }\nReceived:{e.NewStartingIndex}");
+            }
+
+            if (CheckRemove)
+            {
+                if (OldItemsCount != e.OldItems.Count) throw new Exception($"OldItems.Count != Expected Count\nExpected:{OldItemsCount}\nReceived:{e.OldItems.Count}");
+                if (OldStartingIndex != e.OldStartingIndex) throw new Exception($"OldStartingIndex != Expected Index\nExpected:{OldStartingIndex}\nReceived:{e.OldStartingIndex}");
+            }
+
+            DidEventFire = true;
+            SetNextExpectation?.Invoke(this, new EventArgs());
+            SetNextExpectation = null;
+        }
+
+        public void RunTest()
+        {
+            DidEventFire = true;
+            //Add & Remove & Clear
+            AddTests();
+            RemoveTests();
+            //Replace & Sort
+            ReplaceTests();
+            SortTests();
+        }
+
+        private void AddTests()
+        {
+
+            SetAddExpectation();
+            OList.Add(1); // OList now = { 1 }                                                -> 1 items added
+
+            SetAddExpectation(3);
+            OList.AddRange(new int[] { 4, 5, 6 });   // OList now = { 1, 4, 5, 6 }            -> 3 items added
+
+            SetInsertExpectations(1, 0);
+            OList.Insert(0, 0); // OList now = { 0, 1, 4, 5, 6 }                              -> 1 items added, index 0
+            ThrowIfCountUnexpected("InsertTest_1");
+
+            SetInsertExpectations(1, 2);
+            OList.Insert(2, 3); // OList now = { 0, 1, 3, 4, 5, 6 }                           -> 1 items added, index 2
+            ThrowIfCountUnexpected("InsertTest_2");
+
+            SetInsertExpectations(2, 2);
+            OList.InsertRange(2, new int[] { 8, 9 }); // OList now = { 0, 1, 2, 3, 3, 4, 5, 6 }  -> 2 items added
+            ThrowIfCountUnexpected("InsertTest_3");
+
+            int[] test = new int[] { 0, 1, 8, 9, 3, 4, 5, 6 };
+            for (int i = 0; i < test.Length; i++)
+                if (test[i] != OList[i])
+                    throw new Exception("Add Tests Failed - OList does not contain correct values");
+        }
+
+        private void RemoveTests()
+        {
+            SetRemoveExpectation(1, 4);
+            bool success = OList.Remove(3);      // OList now = { 0, 1, 8, 9, 3, 4, 5, 6  }      -> 1 items removed
+
+            SetRemoveExpectation(2, -1);
+            int removedCount = OList.RemoveAll(i => i < 3); // OList now = { 0, 1, 8, 9, 4, 5, 6  }     -> 3 items removed
+
+            DidEventFire = false;
+            bool success2 = OList.Remove(0);     //Should return false, no collection change -> 2 items removed
+            if (DidEventFire) throw new Exception("Event Fired Unexpectedly");
+            DidEventFire = true;
+
+            SetRemoveExpectation(1, 0);
+            OList.RemoveAt(0);                   //OList now = { 8, 9, 4, 5, 6 }                    -> 1 items removed, index 0
+            SetRemoveExpectation(3, 1);
+            OList.RemoveRange(1, 3);             //OList now = { 9, 4, 5, 6  }                          -> 2 items removed, index 0
+
+            if (!(OList.Count == 1 && OList[0] == 9)) 
+                throw new Exception("Add Tests Failed - OList does not contain correct values");
+
+            //ClearTest
+            SetExpectations(NotifyCollectionChangedAction.Reset, 0, 0);
+            OList.Clear();
+            if (!DidEventFire) throw new Exception("Clear()Event Did Not Fired");
+
+        }
+
+        private void ReplaceTests()
+        {
+            SetAddExpectation(7);
+            OList.AddRange(new int[] { 0, 1, 2, 3, 4, 5, 6 });
+
+            SetReplaceExpectation(1, 0);
+            OList[0] = 7;                        // Replace 0 with 7
+            
+            SetExpectations(NotifyCollectionChangedAction.Add, 1, 0, 7);
+            OList[7] = 0;                        // Add 0 to end of the OList
+
+            SetReplaceExpectation(1, 0);
+            OList.Replace(0, 0);                 // Put 0 back at start of OList              -> 1 items replaced, index 0
+            
+            SetReplaceExpectation(1, 7);
+            OList.Replace(7, 7);                 // Put 7 back at end of OList                -> 1 items replaced, index 7
+
+            //Replace 4 items
+            SetReplaceExpectation(4, 3);
+            OList.Replace(3, new int[] { 6, 5, 4, 3 }); // Replace 3,4,5,6 with 6,5,4,3      -> 4 items replaced, index 3
+            
+            // Replace some items, then add the rest
+            SetReplaceExpectation(5, 3);
+            SetNextExpectation += (o, e) => SetExpectations(NotifyCollectionChangedAction.Add, 2, 0);
+            OList.Replace(3, new int[] { 3, 4, 5, 6, 7, 8, 9 }); // Replace 3,4,5,6 with 6,5,4,3      -> 4 items replaced, index 3
+            ThrowIfCountUnexpected("ReplaceTest_2");
+        }
+
+        private void SortTests()
+        {
+            NotifyCollectionChangedAction ResetAction = NotifyCollectionChangedAction.Reset;
+
+            SetMoveExpectation(OList.Count, 0);
+            OList.Reverse();                            //  -> 7 items moved
+            ThrowIfCountUnexpected("ReverseTest");
+
+            SetExpectations(ResetAction, 1, 0);
+            OList.Sort(false);                          //  -> 7 items moved -> 1 event (Reset)
+            ThrowIfCountUnexpected("SortTest_1");
+
+            SetExpectations(ResetAction, 1, 0);
+            OList.Reverse(false);                            //  -> 7 items moved
+            ThrowIfCountUnexpected("ReverseTest_2");
+
+            SetMoveExpectation(OList.Count, 0);
+            OList.Sort(true);             
+            ThrowIfCountUnexpected("SortTest_2");
+        }
+
+    }
+}
---
+++ RoboSharp/ObservableList.cs
@@ -0,0 +1,349 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Extends the Generic <see cref="List{T}"/> class with an event that will fire when the list is updated via standard list methods <para/>
+    /// </summary>
+    /// <typeparam name="T">Type of object the list will contain</typeparam>
+    /// <remarks>
+    /// This class is being provided by the RoboSharp DLL <br/>
+    /// <see href="https://github.com/tjscience/RoboSharp/tree/dev/RoboSharp/ObservableList.cs"/>
+    /// </remarks>
+    public class ObservableList<T> : List<T>, INotifyCollectionChanged
+    {
+        #region < Constructors >
+
+        ///<inheritdoc cref="List{T}()"/>
+        public ObservableList() : base() { }
+
+        ///<inheritdoc cref="List{T}(int)"/>
+        public ObservableList(int capacity) : base(capacity) { }
+
+        ///<inheritdoc cref="List{T}(IEnumerable{T})"/>
+        public ObservableList(IEnumerable<T> collection) : base(collection) { }
+
+        #endregion
+
+        #region < Properties >
+
+        #endregion
+
+        #region < Events >
+
+        /// <summary>
+        /// Raise the <see cref="CollectionChanged"/> event. <para/>
+        /// Override this method to provide post-processing of Added/Removed items within derived classes.
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e) => CollectionChanged?.Invoke(this, e);
+
+        /// <summary> This event fires whenever the List's array is updated. </summary>
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        #endregion
+
+        #region < New Methods >
+
+        /// <summary>
+        /// Replace an item in the list.
+        /// </summary>
+        /// <param name="itemToReplace">Search for this item in the list. If found, replace it. If not found, return false. <paramref name="newItem"/> will not be added to the list. </param>
+        /// <param name="newItem">This item will replace the <paramref name="itemToReplace"/>. If <paramref name="itemToReplace"/> was not found, this item does not get added to the list.</param>
+        /// <returns>True if the <paramref name="itemToReplace"/> was found in the list and successfully replaced. Otherwise false.</returns>
+        public virtual bool Replace(T itemToReplace, T newItem)
+        {
+            if (!this.Contains(itemToReplace)) return false;
+            return Replace(this.IndexOf(itemToReplace), newItem);
+        }
+
+
+        /// <summary>
+        /// Replace an item in the list
+        /// </summary>
+        /// <param name="index">Index of the item to replace</param>
+        /// <param name="newItem">This item will replace the item at the specified <paramref name="index"/></param>
+        /// <returns>True if the the item was successfully replaced. Otherwise throws. </returns>
+        /// <exception cref="IndexOutOfRangeException"/>
+        public virtual bool Replace(int index, T newItem)
+        {
+            this[index] = newItem;
+            return true;
+        }
+
+        /// <summary>
+        /// Replaces the items in this list with the items in supplied collection. If the collection has more items than will be removed from the list, the remaining items will be added to the list. <br/>
+        /// EX: List has 10 items, collection has 5 items, index of 8 is specified (which is item 9 on 0-based index) -> Item 9 + 10 are replaced, and remaining 3 items from collection are added to the list.
+        /// </summary>
+        /// <param name="index">Index of the item to replace</param>
+        /// <param name="collection">Collection of items to insert into the list.</param>
+        /// <returns>True if the the collection was successfully inserted into the list. Otherwise throws.</returns>
+        /// <exception cref="IndexOutOfRangeException"/>
+        /// <exception cref="ArgumentException"/>
+        /// <exception cref="ArgumentNullException"/>
+        public virtual bool Replace(int index, IEnumerable<T> collection)
+        {
+            int collectionCount = collection.Count();
+            int ItemsAfterIndex = this.Count - index;     // # of items in the list after the index
+            int CountToReplace = collectionCount <= ItemsAfterIndex ? collectionCount : ItemsAfterIndex;  //# if items that will be replaced
+            int AdditionalItems = collectionCount - CountToReplace; //# of additional items that will be added to the list.
+
+            List<T> oldItemsList = this.GetRange(index, CountToReplace);
+
+            //Insert the collection
+            base.RemoveRange(index, CountToReplace);
+            if (AdditionalItems > 0)
+                base.AddRange(collection);
+            else
+                base.InsertRange(index, collection);
+
+            List<T> insertedList = collection.ToList();
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItems: insertedList.GetRange(0, CountToReplace), oldItems: oldItemsList, index));
+            if (AdditionalItems > 0)
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, insertedList.GetRange(CountToReplace, AdditionalItems)));
+            return true;
+        }
+
+        #endregion
+
+        #region < Methods that Override List<T> Methods >
+
+        /// <summary>
+        /// Get or Set the element at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to Get or Set.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        new public T this[int index] {
+            get => base[index];
+            set {
+                if (index >= 0 && index < Count)
+                {
+                    //Perform Replace
+                    T old = base[index];
+                    base[index] = value;
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItem: value, oldItem: old, index));
+                }
+                else if (index == Count && index <= Capacity - 1)
+                {
+                    //Append value to end only if the capacity doesn't need to be changed
+                    base.Add(value);
+                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, value, index));
+                }
+                else
+                {
+                    base[index] = value; // Generate ArgumentOutOfRangeException exception
+                }
+            }
+        }
+
+        #region < Add >
+
+        ///<inheritdoc cref="List{T}.Add(T)"/>
+        new public virtual void Add(T item)
+        {
+            base.Add(item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item));
+        }
+
+        ///<inheritdoc cref="System.Collections.Generic.List{T}.AddRange(IEnumerable{T})"/>
+        new public virtual void AddRange(IEnumerable<T> collection)
+        {
+            base.AddRange(collection);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, collection.ToList()));
+        }
+
+        #endregion
+
+        #region < Insert >
+
+        ///<inheritdoc cref="List{T}.Insert(int, T)"/>
+        ///<remarks> Generates <see cref="CollectionChanged"/> event for item that was added and item that was shifted ( Event is raised twice ) </remarks>
+        new public virtual void Insert(int index, T item)
+        {
+            base.Insert(index, item);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, item, index: index));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, this[index + 1], index + 1, index));
+        }
+
+        ///<inheritdoc cref="List{T}.InsertRange(int, IEnumerable{T})"/>
+        ///<remarks> Generates <see cref="CollectionChanged"/> event for items that were added and items that were shifted ( Event is raised twice )</remarks>
+        new public virtual void InsertRange(int index, IEnumerable<T> collection)
+        {
+            int i = index + collection.Count() < this.Count ? collection.Count() : this.Count - index;
+            List<T> movedItems = base.GetRange(index, i);
+            base.InsertRange(index, collection);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, changedItems: collection.ToList(), index));
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, changedItems: movedItems, IndexOf(movedItems[0]), index));
+        }
+
+        #endregion
+
+        #region < Remove >
+
+        ///<inheritdoc cref="List{T}.Clear"/>
+        new public virtual void Clear()
+        {
+            base.Clear();
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        ///<inheritdoc cref="List{T}.Remove(T)"/>
+        new public virtual bool Remove(T item)
+        {
+            if (!base.Contains(item)) return false;
+
+            int i = base.IndexOf(item);
+            if (base.Remove(item))
+            {
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, i));
+                return true;
+            }
+            else
+                return false;
+        }
+
+        ///<inheritdoc cref="List{T}.RemoveAt(int)"/>
+        new public virtual void RemoveAt(int index)
+        {
+            T item = base[index];
+            base.RemoveAt(index);
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, item, index: index));
+        }
+
+        ///<inheritdoc cref="List{T}.RemoveRange(int,int)"/>
+        new public virtual void RemoveRange(int index, int count)
+        {
+            List<T> removedItems = base.GetRange(index, count);
+            if (removedItems.Count > 0)
+            {
+                base.RemoveRange(index, count);
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removedItems.ToList(), index));
+            }
+        }
+
+        ///<inheritdoc cref="List{T}.RemoveAll(Predicate{T})"/>
+        new public virtual int RemoveAll(Predicate<T> match)
+        {
+            List<T> removedItems = base.FindAll(match);
+            int ret = removedItems.Count;
+            if (ret > 0)
+            {
+                ret = base.RemoveAll(match);
+                OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, removedItems));
+            }
+            return ret;
+        }
+
+        #endregion
+
+        #region < Move / Sort >
+
+        #region < Hide base members >
+
+        ///<inheritdoc cref="Sort(int, int, IComparer{T},bool)"/>
+        new virtual public void Sort(int index, int count, IComparer<T> comparer) => Sort(index, count, comparer, true);
+        ///<inheritdoc cref="Sort(IComparer{T}, bool)"/>
+        new virtual public void Sort(IComparer<T> comparer) => Sort(comparer, true);
+        ///<inheritdoc cref="Sort(Comparison{T},bool)"/>
+        new virtual public void Sort(Comparison<T> comparison) => Sort(comparison, true);
+        ///<inheritdoc cref="Sort(bool)"/>
+        new virtual public void Sort() => Sort(true);
+        ///<inheritdoc cref="Reverse(int, int, bool)"/>
+        new virtual public void Reverse(int index, int count) => Reverse(index, count, true);
+        ///<inheritdoc cref="Reverse(bool)"/>
+        new virtual public void Reverse() => Reverse(true);
+
+        #endregion
+
+        ///<inheritdoc cref="List{T}.Reverse()"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Reverse(bool verbose)
+        {
+            PerformMove(new Action(() => base.Reverse()), this, verbose);
+        }
+
+        ///<inheritdoc cref="List{T}.Reverse(int, int)"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Reverse(int index, int count, bool verbose)
+        {
+            List<T> OriginalOrder = base.GetRange(index, count);
+            PerformMove(new Action(() => base.Reverse(index, count)), OriginalOrder, verbose);
+        }
+
+        ///<inheritdoc cref="List{T}.Sort()"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Sort(bool verbose)
+        {
+            PerformMove(new Action(() => base.Sort()), this, verbose);
+        }
+
+        ///<inheritdoc cref="List{T}.Sort(Comparison{T})"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Sort(Comparison<T> comparison, bool verbose)
+        {
+            PerformMove(new Action(() => base.Sort(comparison)), this, verbose);
+        }
+
+        ///<inheritdoc cref="List{T}.Sort(IComparer{T})"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Sort(IComparer<T> comparer, bool verbose)
+        {
+            PerformMove(new Action(() => base.Sort(comparer)), this, verbose);
+        }
+
+        ///<inheritdoc cref="List{T}.Sort(int, int, IComparer{T})"/>
+        ///<inheritdoc cref="PerformMove"/>
+        public virtual void Sort(int index, int count, IComparer<T> comparer, bool verbose)
+        {
+            List<T> OriginalOrder = base.GetRange(index, count);
+            Action action = new Action(() => base.Sort(index, count, comparer));
+            PerformMove(action, OriginalOrder, verbose);
+        }
+
+        /// <remarks>
+        /// Per <see cref="INotifyCollectionChanged"/> rules, generates <see cref="CollectionChanged"/> event for every item that has moved within the list. <br/>
+        /// Set <paramref name="verbose"/> parameter in overload to generate a single <see cref="NotifyCollectionChangedAction.Reset"/> event instead.
+        /// </remarks>
+        /// <param name="MoveAction">Action to perform that will rearrange items in the list - should not add, remove or replace!</param>
+        /// <param name="OriginalOrder">List of items that are intended to rearrage - can be whole or subset of list</param>
+        /// <param name="verbose">
+        /// If TRUE: Create a 'Move' OnCollectionChange event for all items that were moved within the list. <para/>
+        /// If FALSE: Generate a single event with <see cref="NotifyCollectionChangedAction.Reset"/>
+        /// </param>
+        protected void PerformMove(Action MoveAction, List<T> OriginalOrder, bool verbose)
+        {
+            //Store Old List Order
+            List<T> OldIndexList = this.ToList();
+
+            //Perform the move
+            MoveAction.Invoke();
+
+            //Generate the event
+            foreach (T obj in OriginalOrder)
+            {
+                int oldIndex = OldIndexList.IndexOf(obj);
+                int newIndex = this.IndexOf(obj);
+                if (oldIndex != newIndex)
+                {
+                    if (verbose)
+                        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, changedItem: obj, newIndex, oldIndex));
+                    else
+                    {
+                        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+                        break;
+                    }
+                }
+            }
+
+            //OldIndexList no longer needed
+            OldIndexList.Clear();
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
