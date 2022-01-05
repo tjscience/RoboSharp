5c487f40431960c6f02cf4c93b1be008c3f2a5eb
RFBomb <Robbery525@gmail.com>
2022-01-05 12:48:36 -0500

    Add CurrentFile to CopyProgressEventArgs
    Update CopyProgressEventArgs with CurrentFile and CurrentDir properties now that they are available thanks to the ProgressEstimator
    
    Resolves #131

--- RoboSharp/CopyProgressEventArgs.cs
+++ RoboSharp/CopyProgressEventArgs.cs
@@ -7,16 +7,33 @@ namespace RoboSharp
     /// </summary>
     public class CopyProgressEventArgs : EventArgs
     {
-        /// <summary>
-        /// Current File Progress Percentage
-        /// </summary>
-        public double CurrentFileProgress { get; set; }
-
         /// <summary><inheritdoc cref="CopyProgressEventArgs"/></summary>
         /// <param name="progress"><inheritdoc cref="CurrentFileProgress"/></param>
         public CopyProgressEventArgs(double progress)
         {
             CurrentFileProgress = progress;
         }
+
+        /// <summary><inheritdoc cref="CopyProgressEventArgs"/></summary>
+        /// <param name="progress"><inheritdoc cref="CurrentFileProgress"/></param>
+        /// <param name="currentFile"><inheritdoc cref="CurrentFile"/></param>
+        /// <param name="SourceDir"><inheritdoc cref="CurrentDirectory"/></param>
+        public CopyProgressEventArgs(double progress, ProcessedFileInfo currentFile, ProcessedFileInfo SourceDir)
+        {
+            CurrentFileProgress = progress;
+            CurrentFile = currentFile;
+            CurrentDirectory = SourceDir;
+        }
+
+        /// <summary>
+        /// Current File Progress Percentage
+        /// </summary>
+        public double CurrentFileProgress { get; internal set; }
+
+        /// <inheritdoc cref="ProcessedFileInfo"/>
+        public ProcessedFileInfo CurrentFile { get; internal set; }
+
+        /// <summary>Contains information about the Last Directory RoboCopy reported into the log. </summary>
+        public ProcessedFileInfo CurrentDirectory{ get; internal set; }
     }
 }
