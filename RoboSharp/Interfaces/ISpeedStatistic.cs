e16d07960cfec36e2048c55d65ef48000aca4685
RFBomb <Robbery525@gmail.com>
2022-01-12 11:59:49 -0500

    Move ISpeedStatistic Interface
    This also optimized the Event Raising when recalculating the average of the AverageSpeedStatistic object

---
+++ RoboSharp/Interfaces/ISpeedStatistic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using RoboSharp.Results;
+
+namespace RoboSharp.Interfaces
+{
+    /// <summary>
+    /// Provide Read-Only access to a SpeedStatistic
+    /// </summary>
+    public interface ISpeedStatistic : INotifyPropertyChanged, ICloneable
+    {
+        /// <summary> Average Transfer Rate in Bytes/Second </summary>
+        decimal BytesPerSec { get; }
+
+        /// <summary> Average Transfer Rate in MB/Minute</summary>
+        decimal MegaBytesPerMin { get; }
+
+        /// <inheritdoc cref="SpeedStatistic.ToString"/>
+        string ToString();
+
+        /// <returns>new <see cref="SpeedStatistic"/> object </returns>
+        /// <inheritdoc cref="SpeedStatistic.SpeedStatistic(SpeedStatistic)"/>
+        new SpeedStatistic Clone();
+    }
+}
--- RoboSharp/Results/SpeedStatistic.cs
+++ RoboSharp/Results/SpeedStatistic.cs
@@ -1,36 +1,35 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using RoboSharp.Interfaces;
 
 namespace RoboSharp.Results
 {
-    /// <summary>
-    /// Provide Read-Only access to a SpeedStatistic
-    /// </summary>
-    public interface ISpeedStatistic : INotifyPropertyChanged
-    {
-        /// <summary> Average Transfer Rate in Bytes/Second </summary>
-        decimal BytesPerSec { get; }
-
-        /// <summary> Average Transfer Rate in MB/Minute</summary>
-        decimal MegaBytesPerMin { get; }
-
-        /// <inheritdoc cref="SpeedStatistic.ToString"/>
-        string ToString();
-    }
-    
     /// <summary>
     /// Contains information regarding average Transfer Speed. <br/>
     /// Note: Runs that do not perform any copy operations or that exited prematurely ( <see cref="RoboCopyExitCodes.Cancelled"/> ) will result in a null <see cref="SpeedStatistic"/> object.
     /// </summary>
     public class SpeedStatistic : INotifyPropertyChanged, ISpeedStatistic
     {
+        /// <summary>
+        /// Create new SpeedStatistic
+        /// </summary>
+        public SpeedStatistic() { }
+
+        /// <summary>
+        /// Clone a SpeedStatistic
+        /// </summary>
+        public SpeedStatistic(SpeedStatistic stat)
+        {
+            BytesPerSec = stat.BytesPerSec;
+            MegaBytesPerMin = stat.MegaBytesPerMin;
+        }
+
         #region < Private & Protected Members >
 
         private decimal BytesPerSecField = 0;
@@ -46,6 +45,9 @@ namespace RoboSharp.Results
         /// <summary>This event will fire when the value of the SpeedStatistic is updated </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>Raise Property Change Event</summary>
+        protected void OnPropertyChange(string PropertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
         /// <inheritdoc cref="ISpeedStatistic.BytesPerSec"/>
         public virtual decimal BytesPerSec
         {
@@ -55,7 +57,7 @@ namespace RoboSharp.Results
                 if (BytesPerSecField != value)
                 {
                     BytesPerSecField = value;
-                    if (EnablePropertyChangeEvent) PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MegaBytesPerMin"));
+                    if (EnablePropertyChangeEvent) OnPropertyChange("MegaBytesPerMin");
                 }
             }
         }
@@ -69,7 +71,7 @@ namespace RoboSharp.Results
                 if (MegaBytesPerMinField != value)
                 {
                     MegaBytesPerMinField = value;
-                    if (EnablePropertyChangeEvent) PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MegaBytesPerMin"));
+                    if (EnablePropertyChangeEvent) OnPropertyChange("MegaBytesPerMin");
                 }
             }
         }
@@ -85,6 +87,11 @@ namespace RoboSharp.Results
         {
             return $"Speed: {BytesPerSec} Bytes/sec{Environment.NewLine}Speed: {MegaBytesPerMin} MegaBytes/min";
         }
+
+        /// <inheritdoc cref="ISpeedStatistic.Clone"/>
+        public virtual SpeedStatistic Clone() => new SpeedStatistic(this);
+
+        object ICloneable.Clone() => Clone();
 
         internal static SpeedStatistic Parse(string line1, string line2)
         {
@@ -151,6 +158,16 @@ namespace RoboSharp.Results
             Average(speedStats);
         }
 
+        /// <summary>
+        /// Clone an AverageSpeedStatistic
+        /// </summary>
+        public AverageSpeedStatistic(AverageSpeedStatistic stat) : base(stat) 
+        {
+            Divisor = stat.Divisor;
+            Combined_BytesPerSec = stat.BytesPerSec;
+            Combined_MegaBytesPerMin = stat.MegaBytesPerMin;
+        }
+
         #endregion
 
         #region < Fields >
@@ -163,6 +180,13 @@ namespace RoboSharp.Results
 
         /// <summary> Total number of SpeedStats that were combined to produce the Combined_* values </summary>
         private long Divisor = 0;
+
+        #endregion
+
+        #region < Public Methods >
+
+        /// <inheritdoc cref="ICloneable.Clone"/>
+        public override SpeedStatistic Clone() => new AverageSpeedStatistic(this);
 
         #endregion
 
@@ -308,8 +332,19 @@ namespace RoboSharp.Results
 #endif
         internal void CalculateAverage()
         {
-            BytesPerSec = Divisor < 1 ? 0 : Math.Round(Combined_BytesPerSec / Divisor, 3);
-            MegaBytesPerMin = Divisor < 1 ? 0 : Math.Round(Combined_MegaBytesPerMin / Divisor, 3);
+            EnablePropertyChangeEvent = false;
+            //BytesPerSec
+            long i = Divisor < 1 ? 0 : (long)Math.Round(Combined_BytesPerSec / Divisor, 3);
+            bool TriggerBPS = BytesPerSec != i;
+            BytesPerSec = i;
+            //MegaBytes
+            i = Divisor < 1 ? 0 : (long)Math.Round(Combined_MegaBytesPerMin / Divisor, 3);
+            bool TriggerMBPM = MegaBytesPerMin != i;
+            MegaBytesPerMin = i;
+            //Trigger Events
+            EnablePropertyChangeEvent = true;
+            if (TriggerBPS) OnPropertyChange("BytesPerSec");
+            if (TriggerMBPM) OnPropertyChange("MegaBytesPerMin");
         }
 
         /// <summary>
