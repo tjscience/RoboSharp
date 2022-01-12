aadca0b07558318b0999fc78c1025af57ae1b6f6
RFBomb <Robbery525@gmail.com>
2022-01-12 12:49:53 -0500

    Move IRoboCopyCombinedExitStatus

---
+++ RoboSharp/Interfaces/IRoboCopyCombinedExitStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using RoboSharp.Results;
+
+namespace RoboSharp.Interfaces
+{
+    /// <summary>
+    /// Read-Only interface for <see cref="RoboCopyCombinedExitStatus"/>
+    /// </summary>
+    public interface IRoboCopyCombinedExitStatus : INotifyPropertyChanged, ICloneable
+    {
+        /// <inheritdoc cref="RoboCopyCombinedExitStatus.WasCancelled"/>
+        bool WasCancelled { get; }
+
+        /// <inheritdoc cref="RoboCopyCombinedExitStatus.AnyNoCopyNoError"/>
+        bool AnyNoCopyNoError { get; }
+
+        /// <inheritdoc cref="RoboCopyCombinedExitStatus.AnyWasCancelled"/>
+        bool AnyWasCancelled { get; }
+
+        /// <inheritdoc cref="RoboCopyCombinedExitStatus.AllSuccessful"/>
+        bool AllSuccessful { get; }
+
+        /// <inheritdoc cref="RoboCopyCombinedExitStatus.AllSuccessful_WithWarnings"/>
+        bool AllSuccessful_WithWarnings { get; }
+
+    }
+}
