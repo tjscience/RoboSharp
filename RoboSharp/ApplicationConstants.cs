4de9e71e6b3d9bbdb32334a9091027d196674dbf
Darren Rose <darren.rose@pcassistonline.co.uk>
2022-02-26 21:04:08 +0000

    Update ApplicationConstants.cs
    Added additional error code and description

--- RoboSharp/ApplicationConstants.cs
+++ RoboSharp/ApplicationConstants.cs
@@ -6,6 +6,7 @@ namespace RoboSharp
     {
         internal static Dictionary<string, string> ErrorCodes = new Dictionary<string, string>()
         {
+            { "ERROR 33 (0x00000021)", "The process cannot access the file because another process has locked a portion of the file." },
             { "ERROR 32 (0x00000020)", "The process cannot access the file because it is being used by another process." },
             { "ERROR 5 (0x00000005)", "Access is denied." }
         };
