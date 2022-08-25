7771e1e1da6e323f4be6781999e5f31681a24293
RFBomb <robbery525@gmail.com>
2022-08-25 00:01:08 -0400

    Introduce ConsumerHelpers namespace that contains extension methods for help with custom implementations.
    Introduced:
    - extension methods to decide how to treat a file (copy/skip/etc)
    - 2 new interfaces to describe a Dir/File source/Destination pairing, along with a tiny helper class that implements each.
    - SourceDestinationEvaluator object that provides the optimizations (caching) for running the checks against multiple files, to avoid recreating a regex array of wildcard names for example.

---
+++ RoboSharp/ConsumerHelpers/CopyOptionsExtensions.cs
@@ -0,0 +1,158 @@
+﻿using RoboSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RoboSharp.ConsumerHelpers
+{
+    /// <summary>
+    /// Extension Methods for Copy options to assist with custom implementations
+    /// </summary>
+    public static class CopyOptionsExtensions
+    {
+
+        /// <summary>
+        /// Evaluate the pair and determine if it the file at the destination should be purged or not
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="pair"></param>
+        /// <returns>TRUE if destination file should be deleted, otherwise false.</returns>
+        public static bool ShouldPurge(this IRoboCommand command, IFileSourceDestinationPair pair)
+        {
+            bool destExists = pair.Destination.Exists;
+            bool sourceExists = pair.Source.Exists;
+            if ((command.CopyOptions.Mirror | command.CopyOptions.Purge) && destExists && !sourceExists)
+                return command.SelectionOptions.ExcludeExtra;
+            else
+                return false;
+        }
+
+        #region < IncludedFiles >
+
+        /// <summary>
+        /// Determine if the file should be rejected based on its filename by checking against the FileFilter
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="fileName">filename to compare</param>
+        /// <param name="inclusionCollection">
+        /// The collection of regex objects to compare against - If this is null, a new array will be generated from <see cref="CopyOptions.FileFilter"/>. <br/>
+        /// ref is used for optimization during the course of the run, to avoid creating regex for every file check.
+        /// </param>
+        /// <returns></returns>
+        public static bool ShouldIncludeFileName(this CopyOptions options, string fileName, ref Regex[] inclusionCollection)
+        {
+            if (inclusionCollection is null)
+            {
+                List<Regex> reg = new List<Regex>();
+                foreach (string s in options.FileFilter)
+                {
+                    reg.Add(SelectionOptionsExtensions.SanitizeFileNameRegex(s));
+                }
+                inclusionCollection = reg.ToArray();
+            }
+            if (inclusionCollection.Length == 0) return false;
+            return inclusionCollection.Any(r => r.IsMatch(fileName));
+        }
+
+        /// <inheritdoc cref="ShouldIncludeFileName(CopyOptions, string, ref Regex[])"/>
+        public static bool ShouldIncludeFileName(this CopyOptions options, FileInfo Source, ref Regex[] inclusionCollection) => ShouldIncludeFileName(options, Source.Name, ref inclusionCollection);
+
+        /// <inheritdoc cref="ShouldIncludeFileName(CopyOptions, string, ref Regex[])"/>
+        public static bool ShouldIncludeFileName(this CopyOptions options, IFileSourceDestinationPair comparer, ref Regex[] inclusionCollection) => ShouldIncludeFileName(options, comparer.Source.Name, ref inclusionCollection);
+
+        #endregion
+
+        #region < SetAttributes >
+
+        /// <summary>
+        /// Parses <see cref="CopyOptions.AddAttributes"/> and <see cref="CopyOptions.RemoveAttributes"/> and applies them to the destination. <br/> Any attribute addition comes before attribute removal.
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="destination"></param>
+        public static void SetFileAttributes(this CopyOptions options, FileInfo destination)
+        {
+            var Addattr = SelectionOptions.ConvertFileAttrStringToEnum(options.AddAttributes);
+            var Subattr = SelectionOptions.ConvertFileAttrStringToEnum(options.RemoveAttributes);
+            if (Addattr.HasValue)
+                destination.Attributes &= Addattr.Value;
+            if (Subattr.HasValue)
+                destination.Attributes &= ~Subattr.Value;
+        }
+
+        /// <inheritdoc cref="SetFileAttributes(CopyOptions, FileInfo)"/>
+        public static void SetFileAttributes(this CopyOptions options, string destination)
+        {
+            SetFileAttributes(options, new FileInfo(destination));
+        }
+
+        /// <inheritdoc cref="SetFileAttributes(CopyOptions, FileInfo)"/>
+        public static void SetFileAttributes(this CopyOptions options, IFileSourceDestinationPair comparer)
+        {
+            SetFileAttributes(options, comparer.Destination);
+            
+        }
+
+        #endregion
+
+        #region < AddAttributes >
+
+        /// <summary>
+        /// Add the attributes to the destination
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="destination"></param>
+        public static void AddFileAttributes(this CopyOptions options, FileInfo destination)
+        {
+            var attr = SelectionOptions.ConvertFileAttrStringToEnum(options.AddAttributes);
+            if (attr is null) return;
+            destination.Attributes &= attr.Value;
+        }
+
+        /// <inheritdoc cref="AddFileAttributes(CopyOptions, FileInfo)"/>
+        public static void AddFileAttributes(this CopyOptions options, string destination)
+        {
+            AddFileAttributes(options, new FileInfo(destination));
+        }
+
+        /// <inheritdoc cref="AddFileAttributes(CopyOptions, FileInfo)"/>
+        public static void AddFileAttributes(this CopyOptions options, IFileSourceDestinationPair comparer)
+        {
+            AddFileAttributes(options, comparer.Destination);
+        }
+
+        #endregion
+
+        #region < RemoveAttributes >
+
+        /// <summary>
+        /// Removes the attributes from the destination
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="destination"></param>
+        public static void RemoveFileAttributes(this CopyOptions options, FileInfo destination)
+        {
+            var attr = SelectionOptions.ConvertFileAttrStringToEnum(options.RemoveAttributes);
+            if (attr is null) return;
+            destination.Attributes &= ~attr.Value;
+        }
+
+        /// <inheritdoc cref="RemoveFileAttributes(CopyOptions, FileInfo)"/>
+        public static void RemoveFileAttributes(this CopyOptions options, string destination)
+        {
+            RemoveFileAttributes(options, new FileInfo(destination));
+        }
+
+        /// <inheritdoc cref="RemoveFileAttributes(CopyOptions, FileInfo)"/>
+        public static void RemoveFileAttributes(this CopyOptions options, IFileSourceDestinationPair comparer)
+        {
+            RemoveFileAttributes(options, comparer.Destination);
+        }
+
+        #endregion
+
+    }
+}
---
+++ RoboSharp/ConsumerHelpers/ISourceDestinationPair.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboSharp.ConsumerHelpers
+{
+    /// <summary>
+    /// Interface used for extension methods for RoboSharp custom implementations that has File Source/Destination info
+    /// </summary>
+    public interface IFileSourceDestinationPair
+    {
+        /// <summary>
+        /// Source File Information
+        /// </summary>
+        public FileInfo Source { get; }
+
+        /// <summary>
+        /// Destination FIle Information
+        /// </summary>
+        public FileInfo Destination { get; }
+    }
+
+    /// <summary>
+    /// Helper Class that implements the <see cref="IFileSourceDestinationPair"/> interface
+    /// </summary>
+    public class FileSourceDestinationPair : IFileSourceDestinationPair
+    {
+        /// <summary></summary>
+        public FileSourceDestinationPair(FileInfo source, FileInfo destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+        /// <inheritdoc/>
+        public FileInfo Source { get; }
+        /// <inheritdoc/>
+        public FileInfo Destination { get; }
+    }
+
+    /// <summary>
+    /// Interface used for extension methods for RoboSharp custom implementations that has File Source/Destination info
+    /// </summary>
+    public interface IDirSourceDestinationPair
+    {
+        /// <summary>
+        /// Source File Information
+        /// </summary>
+        public DirectoryInfo Source { get; }
+
+        /// <summary>
+        /// Destination FIle Information
+        /// </summary>
+        public DirectoryInfo Destination { get; }
+    }
+
+    /// <summary>
+    /// Helper Class that implements the <see cref="IDirSourceDestinationPair"/> interface
+    /// </summary>
+    public class DirSourceDestinationPair : IDirSourceDestinationPair
+    {
+        /// <summary></summary>
+        public DirSourceDestinationPair(DirectoryInfo source, DirectoryInfo destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
+        /// <inheritdoc/>
+        public DirectoryInfo Source { get; }
+        /// <inheritdoc/>
+        public DirectoryInfo Destination { get; }
+    }
+}
