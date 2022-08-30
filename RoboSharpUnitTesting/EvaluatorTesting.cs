cf0e7808fc5f053f806f75879bf3911e901df594
RFBomb <Robbery525@gmail.com>
2022-08-30 12:00:59 -0400

    Update EvaluatorTesting.cs
    Simplified by using the extension methods

--- RoboSharpUnitTesting/EvaluatorTesting.cs
+++ RoboSharpUnitTesting/EvaluatorTesting.cs
@@ -26,31 +26,13 @@ namespace RoboSharpUnitTesting
             var estimator = new ProgressEstimator(this);
             DirectoryInfo source = new DirectoryInfo(base.CopyOptions.Source);
             DirectoryInfo dest = new DirectoryInfo(base.CopyOptions.Destination);
-            DirSourceDestinationPair SourceDestPair = new DirSourceDestinationPair(source, dest);
-
-            FileSourceDestinationPair CreateFilePair(FileInfo sourcefile)
-            {
-                return new FileSourceDestinationPair(sourcefile, new FileInfo(source.FullName.Replace(source.FullName, dest.FullName)));
-            }
-            DirSourceDestinationPair CreateDirPair(DirectoryInfo dir)
-            {
-                return new DirSourceDestinationPair(dir, new DirectoryInfo(source.FullName.Replace(source.FullName, dest.FullName)));
-            }
+            DirectorySourceDestinationPair SourceDestPair = new DirectorySourceDestinationPair(source, dest);
 
             // Recursive routine that digs into the folder structure
-            void Dig(DirSourceDestinationPair dirs)
+            void Dig(DirectorySourceDestinationPair dirs)
             {
                 //Get the files in each directory
-                List<FileSourceDestinationPair> pairs = new List<FileSourceDestinationPair>();
-                foreach (FileInfo f in dirs.Source.GetFiles())
-                    pairs.Add(CreateFilePair(f));
-                if (dirs.Destination.Exists)
-                    foreach (FileInfo f in dirs.Destination.GetFiles())
-                        if (pairs.Any(d => d.Destination.FullName == f.FullName))
-                        { }
-                        else pairs.Add(CreateFilePair(f));
-
-                foreach (var pair in pairs)
+                foreach (var pair in dirs.GetFilePairsEnumerable())
                 {
                     bool shouldCopy = evaluator.ShouldCopyFile(pair, out ProcessedFileInfo info);
                     estimator.AddFile(info, true);
@@ -63,16 +45,7 @@ namespace RoboSharpUnitTesting
                 }
 
                 //Get the directories in source and destination
-                List<DirSourceDestinationPair> dirPairs = new List<DirSourceDestinationPair>();
-                foreach (DirectoryInfo f in dirs.Source.GetDirectories())
-                    dirPairs.Add(CreateDirPair(f));
-                if (dirs.Destination.Exists)
-                    foreach (DirectoryInfo f in dirs.Destination.GetDirectories())
-                        if (dirPairs.Any(d => d.Destination.FullName == f.FullName))
-                        { }
-                        else dirPairs.Add(CreateDirPair(f));
-
-                foreach (var pair in dirPairs)
+                foreach (var pair in dirs.GetDirectoryPairsEnumerable())
                 {
                     bool shouldDig = evaluator.ShouldCopyDir(pair, out var info);
                     estimator.AddDir(info);
