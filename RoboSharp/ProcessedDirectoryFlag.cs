739141cbb7cfbf281439c947f01580552af5fa84
RFBomb <20431767+RFBomb@users.noreply.github.com>
2023-09-25 22:48:40 -0400

    ProcessedFileInfo Constructors
    - Add Constructors to ProcessedFileInfo
    - Add ProcessedDirectoryflag and ProcessedFileFlag to support use of those constructors.
    - Add some helper methods to the RoboSharpConfiguration to facilitate grabbing the correct strings for the ProcessedFileInfo object.
    - Override the ToString() method to put data out in same structure as RoboCopy

---
+++ RoboSharp/ProcessedDirectoryFlag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboSharp
+{
+    /// <summary>
+    /// The Directory Classes available within the configuration
+    /// </summary>
+    public enum ProcessedDirectoryFlag
+    {
+        /// <summary>
+        /// String.Empty
+        /// </summary>
+        None,
+        /// <inheritdoc cref="RoboSharpConfiguration.LogParsing_DirectoryExclusion"/>
+        Exclusion,
+        /// <inheritdoc cref="RoboSharpConfiguration.LogParsing_ExistingDir"/>
+        ExistingDir,
+        /// <inheritdoc cref="RoboSharpConfiguration.LogParsing_ExtraDir"/>
+        ExtraDir,
+        /// <inheritdoc cref="RoboSharpConfiguration.LogParsing_NewDir"/>
+        NewDir,
+    }
+}
