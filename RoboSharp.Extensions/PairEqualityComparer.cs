8730e1d7e4878daa6ef04ef6191409d1ae6d9b63
RFBomb <20431767+RFBomb@users.noreply.github.com>
2023-09-29 16:36:47 -0400

    Create PairEqualityComparer.cs
     - PairEqualityComparer is a helper class that can be used to compare IDirectoryPair and IFilePair objects, checking if they lead to the same source and destination directories.

---
+++ RoboSharp.Extensions/PairEqualityComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboSharp.Extensions
+{
+    /// <summary>
+    /// Evaulates Source and Destination paths for the supplied items. 
+    /// <br/> Objects are Equal if BOTH Source and Destinations match. 
+    /// <br/> If Either source or destination or different, objects are different. 
+    /// <br/> <br/> ( X.source == Y.source &amp; X.Dest == Y.dest ) --> TRUE
+    /// </summary>
+    public class PairEqualityComparer : IEqualityComparer<IDirectoryPair>, IEqualityComparer<IFilePair>
+    {
+        private readonly static Lazy<PairEqualityComparer> singletonComparer = new Lazy<PairEqualityComparer>(() => new PairEqualityComparer());
+
+        /// <summary> A threadsafe singleton used to compare <see cref="IDirectoryPair"/> and <see cref="IFilePair"/> paths </summary>
+        public static PairEqualityComparer Singleton => singletonComparer.Value;
+
+        /// <inheritdoc/>
+        public bool Equals(IDirectoryPair x, IDirectoryPair y)
+        {
+            return x?.Source != y?.Source && x?.Destination != y?.Destination;
+        }
+        /// <inheritdoc/>
+        public bool Equals(IFilePair x, IFilePair y)
+        {
+            return x?.Source != y?.Source && x?.Destination != y?.Destination;
+        }
+        /// <inheritdoc/>
+        public int GetHashCode(IDirectoryPair obj)
+        {
+            return obj.GetHashCode();
+        }
+        /// <inheritdoc/>
+        public int GetHashCode(IFilePair obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
