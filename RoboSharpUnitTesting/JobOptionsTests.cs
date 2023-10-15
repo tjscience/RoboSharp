0e4bafbc2892b0e87e448024356eb722318dcaa9
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2023-10-15 09:59:31 -0400

    Update JobOptionsTests.cs
     - Add some unit tests

--- RoboSharpUnitTesting/JobOptionsTests.cs
+++ RoboSharpUnitTesting/JobOptionsTests.cs
@@ -9,6 +9,8 @@ namespace RoboSharp.UnitTests
     [TestClass]
     public class JobOptionsTests
     {
+        private static string GetJobFilePath() => Path.Combine(Path.GetDirectoryName(Test_Setup.Source_Standard), "JobFileTesting", "TestJobFile.rcj");
+
         /// <summary>
         /// This test ensures that the destination directory is not created when using the /QUIT function
         /// </summary>
@@ -37,5 +39,42 @@ namespace RoboSharp.UnitTests
                     try { Directory.Delete(cmd.CopyOptions.Destination); } catch { }
             }
         }
+		
+		
+		[TestMethod]
+        public async Task Test_SaveJobFile() 
+		{
+            RoboCommand cmd = new RoboCommand(source: Test_Setup.Source_Standard, destination: Path.Combine(Test_Setup.TestDestination, Path.GetRandomFileName()));
+            await cmd.SaveAsJobFile(GetJobFilePath());
+            Assert.IsTrue(File.Exists(GetJobFilePath()), "\n Job File was not saved.");
+		}
+
+        [DataRow(null, DisplayName = "Bad File Extension")]
+        [DataRow("ZZ:\\SomeDestination\\BadFile.rcj", DisplayName = "Bad Input String")]
+        [TestMethod]
+        public async Task Test_SaveJobFileError(string savePath)
+        {
+            RoboCommand cmd = new RoboCommand(source: Test_Setup.Source_Standard, destination: Path.Combine(Test_Setup.TestDestination, Path.GetRandomFileName()));
+            bool errorRaised = false;
+            cmd.OnCommandError += (o, e) =>
+            {
+                Console.WriteLine(e.Error);
+                Console.WriteLine(e.Exception);
+                errorRaised = true;
+            };
+            if (savePath is null) savePath = Path.ChangeExtension(GetJobFilePath(), ".XYZ");
+            await cmd.SaveAsJobFile(savePath);
+            Assert.IsTrue(errorRaised);
+        }
+
+        [TestMethod]
+        public async Task Test_LoadJobFile()
+        {
+            await (Test_SaveJobFile());
+            RoboCommand cmd = RoboCommand.LoadFromJobFile(GetJobFilePath());
+            Assert.IsNotNull(cmd);
+        }
+
+        
     }
 }
