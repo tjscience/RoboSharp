0c18ff99353af9552f5af58e5aeb73b5785dc9ae
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2024-02-16 13:57:24 -0500

    Fix robo command ambiguity (#191)
    * Update Constructors that that had optional bool
    
    * Reduce Ambiguity

--- RoboSharp/RoboCommand.cs
+++ RoboSharp/RoboCommand.cs
@@ -34,6 +34,13 @@ namespace RoboSharp
             Init();
         }
 
+        /// <inheritdoc cref="RoboCommand.RoboCommand(string, string, CopyActionFlags, SelectionFlags, LoggingFlags)"/>
+        public RoboCommand(string source, string destination, string name, CopyActionFlags copyActionFlags, SelectionFlags selectionFlags = SelectionFlags.Default, LoggingFlags loggingFlags = LoggingFlags.RoboSharpDefault)
+            :this(source, destination, copyActionFlags, selectionFlags, loggingFlags)
+        {
+            Name = name;
+        }
+
         /// <summary>
         /// Create a new RoboCommand object with the provided settings.
         /// </summary>
@@ -50,21 +57,22 @@ namespace RoboSharp
         }
 
         /// <inheritdoc cref="Init"/>
-        public RoboCommand(string name, bool stopIfDisposing = true)
-        {
-            InitClassProperties();
-            Init(name, stopIfDisposing);
-        }
+        public RoboCommand(string name) : this(string.Empty, string.Empty, name, stopIfDisposing: true) { }
 
         /// <inheritdoc cref="Init"/>
-        public RoboCommand(string source, string destination, bool stopIfDisposing = true)
-        {
-            InitClassProperties();
-            Init("", stopIfDisposing, source, destination);
-        }
+        public RoboCommand(string name, bool stopIfDisposing) : this(string.Empty, string.Empty, name, stopIfDisposing) { }
 
         /// <inheritdoc cref="Init"/>
-        public RoboCommand(string source, string destination, string name, bool stopIfDisposing = true)
+        public RoboCommand(string source, string destination) : this(source, destination, string.Empty, stopIfDisposing: true) { }
+
+        /// <inheritdoc cref="Init"/>
+        public RoboCommand(string source, string destination, bool stopIfDisposing) : this(source, destination, string.Empty, stopIfDisposing) { }
+
+        /// <inheritdoc cref="Init"/>
+        public RoboCommand(string source, string destination, string name) : this(source, destination, name, StopIfDisposing: true) { }
+
+        /// <inheritdoc cref="Init"/>
+        public RoboCommand(string source, string destination, string name, bool stopIfDisposing)
         {
             InitClassProperties();
             Init(name, stopIfDisposing, source, destination);
