9e32377a619e3d1584726ab09160d052c8e0ad8a
RFBomb <20431767+RFBomb@users.noreply.github.com>
2024-02-26 16:08:17 -0500

    Update CopyFileOptions.cs
    - Revised the order in which the values appear to match the Microsoft documentation page as of writing time.
    - Removed options that were introduced when ChatGPT mocked everything up, as these options don't exist in the Microsoft docs.

--- RoboSharp.Extensions/CopyFileEx/CopyFileOptions.cs
+++ RoboSharp.Extensions/CopyFileEx/CopyFileOptions.cs
@@ -16,23 +16,6 @@ namespace RoboSharp.Extensions.CopyFileEx
         NONE = 0x0,
 
         /// <summary>
-        /// The copy operation fails immediately if the target file already exists. 
-        /// </summary>
-        FAIL_IF_EXISTS = 0x00000001,
-
-        /// <summary>
-        /// Progress of the copy is tracked in the target file in case the copy fails. ( Required for Progress Reporting )
-        /// <br/> - The failed copy can be restarted at a later time by specifying the same values for lpExistingFileName and lpNewFileName as those used in the call that failed. 
-        /// <br/> - This can significantly slow down the copy operation as the new file may be flushed multiple times during the copy operation. 
-        /// </summary>
-        RESTARTABLE = 0x00000002,
-
-        /// <summary>
-        /// The file is copied and the original file is opened for write access. 
-        /// </summary>
-        OPEN_SOURCE_FOR_WRITE = 0x00000004,
-
-        /// <summary>
         /// An attempt to copy an encrypted file will succeed even if the destination copy cannot be encrypted. 
         /// </summary>
         ALLOW_DECRYPTED_DESTINATION = 0x00000008,
@@ -44,36 +27,32 @@ namespace RoboSharp.Extensions.CopyFileEx
         COPY_SYMLINK = 0x00000800,
 
         /// <summary>
+        /// The copy operation fails immediately if the target file already exists. 
+        /// </summary>
+        FAIL_IF_EXISTS = 0x00000001,
+
+        /// <summary>
         /// The copy operation is performed using unbuffered I/O, bypassing system I/O cache resources. Recommended for very large file transfers.
         /// <para/>Windows Server 2003 and Windows XP:  This value is not supported
         /// </summary>
         NO_BUFFERING = 0x00001000,
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        REQUEST_SECURITY_PRIVILEGES = 0x00002000,
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        RESUME_FROM_PAUSE = 0x00004000,
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        NO_OFFLOAD = 0x00040000,
 
         /// <summary>
-        /// 
+        /// The file is copied to the destination. Once complete, the original file is opened for write access.
         /// </summary>
-        IGNORE_EDP_BLOCK = 0x00400000,
+        OPEN_SOURCE_FOR_WRITE = 0x00000004,
+
+        /// <summary>
+        /// Progress of the copy is tracked in the target file in case the copy fails.
+        /// <br/> - The failed copy can be restarted at a later time by specifying the same values for lpExistingFileName and lpNewFileName as those used in the call that failed. 
+        /// <br/> - This can significantly slow down the copy operation as the new file may be flushed multiple times during the copy operation. 
+        /// </summary>
+        RESTARTABLE = 0x00000002,
 
         /// <summary>
         /// Request the underlying transfer channel compress the data during the copy operation. The request may not be supported for all mediums, in which case it is ignored. The compression attributes and parameters (computational complexity, memory usage) are not configurable through this API, and are subject to change between different OS releases.
         /// <para/>This flag was introduced in Windows 10, version 1903 and Windows Server 2022. On Windows 10, the flag is supported for files residing on SMB shares, where the negotiated SMB protocol version is SMB v3.1.1 or greater.
         /// </summary>
         REQUEST_COMPRESSED_TRAFFIC = 0x10000000,
-
     }
 }
