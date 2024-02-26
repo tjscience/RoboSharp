204388b594b98bf6fd1fbda9a311e615f007d8ae
RFBomb <20431767+RFBomb@users.noreply.github.com>
2024-02-26 10:01:08 -0500

    Create Helpers for wrapping CopyFileEx

---
+++ RoboSharp.Extensions/CopyFileEx/MoveFileOptions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace RoboSharp.Extensions.CopyFileEx
+{
+    /// <summary>
+    /// The Options that can be used with MoveFileWithProgressA
+    /// </summary>
+    /// <remarks><see href="https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-movefilewithprogressa"/></remarks>
+    [DefaultValue(Default)]
+    [Flags]
+    public enum MoveFileOptions : uint
+    {
+        /// <summary>
+        /// No options specified - Note that this is not the default used by the library.
+        /// </summary>
+        None = 0x0,
+
+        /// <summary>
+        /// Default Recommended options to ensure it works across drives and doesn't return until file move completed.
+        /// </summary>
+        Default = MoveFileOptions.COPY_ALLOWED | WRITE_THROUGH,
+
+        /// <summary>
+        /// If a file exists at the destination, overwrite it.
+        /// <br/> - This option is only valid if the Source and Destination are both files (the destination being a directory path won't work)
+        /// </summary>
+        REPLACE_EXISTSING = 0x00000001,
+
+        /// <summary>
+        /// If the file is to be moved to a different volume, the function simulates the move by using the CopyFile and DeleteFile functions.
+        /// <br/> - If the file is successfully copied to a different volume and the original file is unable to be deleted, the function succeeds leaving the source file intact.
+        /// <br/> - This value cannot be used with MOVEFILE_DELAY_UNTIL_REBOOT.
+        /// </summary>
+        COPY_ALLOWED = 0x00000002,
+
+        /// <summary>
+        /// The system does not move the file until the operating system is restarted. The system moves the file immediately after AUTOCHK is executed, but before creating any paging files. 
+        /// Consequently, this parameter enables the function to delete paging files from previous startups.
+        /// <br/> - This value can only be used if the process is in the context of a user who belongs to the administrators group or the LocalSystem account.
+        /// <br/> - This value cannot be used with MOVEFILE_COPY_ALLOWED.
+        /// </summary>
+        DELAY_UNTIL_REBOOT = 0x00000004,
+
+        /// <summary>
+        /// The function does not return until the file has actually been moved on the disk. 
+        /// <br/> - Setting this value guarantees that a move performed as a copy and delete operation is flushed to disk before the function returns. The flush occurs at the end of the copy operation. 
+        /// <br/> - This value has no effect if MOVEFILE_DELAY_UNTIL_REBOOT is set.
+        /// </summary>
+        WRITE_THROUGH = 0x00000008,
+
+        /// <summary>
+        /// Reserved for future use. 
+        /// </summary>
+        CREATE_HARDLINK = 0x00000010,
+
+        /// <summary>
+        /// The function fails if the source file is a link source, but the file cannot be tracked after the move. 
+        /// This situation can occur if the destination is a volume formatted with the FAT file system.
+        /// </summary>
+        FAIL_IF_NOT_TRACKABLE = 0x00000020
+    }
+}
---
+++ RoboSharp.Extensions/CopyFileEx/Win32Error.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace RoboSharp.Extensions.CopyFileEx
+{
+    /// <summary>
+    /// Class to work with the last Win32 Error
+    /// </summary>
+    public static class Win32Error
+    {
+#if Net6OrGreater
+        /// <inheritdoc cref="Marshal.GetLastPInvokeError"/>
+        /// <remarks>
+        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.marshal.getlastpinvokeerror?view=net-8.0"/>
+        /// </remarks>
+        public static int GetLastErrorCode() => Marshal.GetLastPInvokeError();
+#else
+        /// <inheritdoc cref="Marshal.GetLastWin32Error"/>
+        /// <remarks>
+        /// On .NET Framework, the GetLastWin32Error method can retain error information from one P/Invoke call to the next. 
+        /// <br/>On .NET Core, error information is cleared before P/Invoke call, and the GetLastWin32Error represents only error information from the last method call
+        /// <para/><seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.marshal.getlastwin32error?view=net-8.0"/>
+        /// </remarks>
+        public static int GetLastErrorCode() => Marshal.GetLastWin32Error();
+#endif
+
+        /// <inheritdoc cref="ThrowLastError(string,string)"/>
+        [DebuggerHidden]
+        public static void ThrowLastError()
+        {
+            var e = GetLastError();
+            if (e is Exception er) throw er;
+        }
+
+        /// <summary>Gets the last Exception then throws it. If no exception was detected, returns to caller.</summary>
+        /// <returns/>
+        /// <inheritdoc cref="GenerateException(string, string, int)"/>
+        [DebuggerHidden]
+        public static void ThrowLastError(string source, string destinaation)
+        {
+            var e = GetLastError(source, destinaation);
+            if (e is Exception er) throw er;
+        }
+
+
+        /// <inheritdoc cref="GetLastError(string, string)"/>
+        public static Exception GetLastError() => GenerateException(string.Empty, string.Empty, GetLastErrorCode());
+
+        ///<summary>
+        /// Get the last Win32 error code and generate the appropriate exception.
+        /// </summary>
+        /// <inheritdoc cref="GenerateException(string, string, int)"/>
+        public static Exception GetLastError(string source, string destination) => GenerateException(source, destination, GetLastErrorCode());
+
+        ///<summary>
+        /// Look up the Win32 error code and generate the appropriate exception.
+        /// </summary>
+        /// <remarks>
+        /// <see href="https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/18d8fbe8-a967-4f1c-ae50-99ca8e491d2d"/>
+        /// </remarks>
+        /// <returns>null if the <paramref name="errorCode"/> == 0, otherwise an Exception object</returns>
+        public static Exception GenerateException(string sourceFile, string destFile, int errorCode)
+        {
+
+            if (errorCode == 0) return null; //No Error
+            Exception e = errorCode switch
+            {
+                80 => new IOException("The destination file already exists."),
+                0x000004D3 => new OperationCanceledException("The operation was cancelled."),
+                1 => new InvalidOperationException("Invalid Operation"),
+                2 => new FileNotFoundException(message: "Unable to locate the file", fileName: sourceFile),
+                3 => new DirectoryNotFoundException("Unable to locate the directory: " + Path.GetDirectoryName(destFile)),
+                4 => new FileNotFoundException("Too Many Open Files. Unable to open the file: ", sourceFile),
+                5 => new UnauthorizedAccessException("Access Denied"),
+                6 => new IOException("File Handle is Invalid"),
+                7 => new IOException("The Storage Control Blocks were destroyed"),
+                8 => new InsufficientMemoryException("Not enough storage is available to process this command."),
+                0x0000000E => new InsufficientMemoryException("Not enough storage is available to complete this operation."),
+                0x0000000F => new DriveNotFoundException("The system cannot find the drive specified."),
+                0x00000013 => new UnauthorizedAccessException("The media is write-protected."),
+                0x00000014 => new DriveNotFoundException("The system cannot find the device specified."),
+                0x00000015 => new IOException("The device is not ready."),
+                0x00000021 => new IOException("The process cannot access the file because another process has locked a portion of the file."),
+                0x00000027 => new IOException("The destination disk is full: " + Path.GetPathRoot(destFile)),
+                0x00000032 => new IOException("The destination file is locked"), // Occurs when the file is locked
+                0x0000006E => new IOException("The system cannot open the device or file specified"),
+                0x0000006F => new IOException("The file name is too long."),
+                0x00000070 => new IOException("There is not enough space on the disk."),
+                _ => new IOException($@"CopyFileEx Error Code: {errorCode}{Environment.NewLine} See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/18d8fbe8-a967-4f1c-ae50-99ca8e491d2d for details")
+            };
+
+            e.Data.Add("Source", sourceFile);
+            e.Data.Add("Destination", destFile);
+            e.Data.Add("Win32ErrorCode", errorCode);
+            e.Data.Add("Help Page", @"https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/18d8fbe8-a967-4f1c-ae50-99ca8e491d2d");
+            return e;
+        }
+    }
+}
