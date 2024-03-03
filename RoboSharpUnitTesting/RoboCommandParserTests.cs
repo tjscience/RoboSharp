18668fe9f3755f7a25d069200b8209fc62445fa0
Robert Brenckman <20431767+RFBomb@users.noreply.github.com>
2024-03-03 11:56:24 -0500

    Update RoboCommandParserTests.cs

--- RoboSharpUnitTesting/RoboCommandParserTests.cs
+++ RoboSharpUnitTesting/RoboCommandParserTests.cs
@@ -259,7 +259,7 @@ namespace RoboSharp.UnitTests
             // Note : Due to how RoboCommand prints out file filters, ensure input file filters are always quoted
             IRoboCommand cmdResult = RoboCommandParser.Parse(input);
             cmdResult.LoggingOptions.PrintSizesAsBytes = false;
-            string expected = input += " /R:0"; // robocommand ALWAYS prints these values
+            string expected = input + " /R:0"; // robocommand ALWAYS prints these values
 
             Assert.AreEqual(expected.Trim(), cmdResult.ToString().Trim(), $"\n\nProduced Command is not equal!\nExpected:\t{expected}\n  Result:\t{cmdResult}"); // Final test : both should produce the same ToString()
             Console.WriteLine($"\n\n Input : {input}");
